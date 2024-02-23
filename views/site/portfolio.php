576b12b0726f253c5bdcc4e2e2b0d4e71cd8fd56
raghvandr <78319113+raghvandr@users.noreply.github.com>
2024-02-23 14:28:54 +0530

    code

---
+++ views/site/portfolio.php
@@ -0,0 +1,324 @@
+<?php  
+/** @var yii\web\View $this */
+
+$this->title = 'Portfolio';
+?>
+<!-- <body class="index-page" data-bs-spy="scroll" data-bs-target="#navmenu"> -->
+
+  <link rel="stylesheet" href= 
+        "https://unpkg.com/aos@next/dist/aos.css" /> 
+  
+    <script src= 
+        "https://unpkg.com/aos@next/dist/aos.js"> 
+    </script> 
+    <!-- Hero Section - Home Page -->
+    <br><br><br><br>
+
+   
+
+    <!-- About Section - Home Page -->
+    <section id="about" class="about">
+      <div class="container">
+        <div class="row gy-2 align-items-stretch justify-content-between features-item text-justify ">
+        
+        <div class="col-lg-9 d-flex justify-content-center flex-column" data-aos="fade-up" >
+          <h3 class="text-danger">Automated Healthcare Web App(Private Domain)</h3>
+                    <p>
+                      A healthcare web application developed by our team at Tech Savvy Solutions to revolutionize the process of healthcare claims settlement. It combines advanced artificial intelligence (AI) models with a DAG-based (Directed Acyclic Graph) pipelining system to streamline and automate the claims settlement process in the healthcare domain.
+                    </p>
+                    <h3 class="text-danger">
+                      Key Features:
+                    </h3>
+                    
+                    <p><strong class="text-danger">AI Model Integration:</strong>
+                      Analyzes healthcare claims data and facilitates automated decision-making.
+                    </p>
+                    <p> <strong class="text-danger">DAG-Based Pipelining:</strong>
+                      Orchestrates data flow and processes for optimal performance.
+                    </p>
+                    
+                    <p><strong class="text-danger">Automated Claim Processing:</strong>
+                       From submission to adjudication, Enso speeds up processing with minimal errors. 
+                    </p>
+                    <p><strong class="text-danger">Real-Time Monitoring:</strong>
+                      Provides insights into system performance and efficiency. 
+                    </p>
+                    <p><strong class="text-danger">Scalability and Customization:</strong>
+                      Adaptable to different healthcare organization needs.
+                    </p>
+                    <p>
+                      <strong>ML Application:</strong>  Empowering Business Users in Model Development and Monitoring. <br>
+                      Our ML Application empowers business users to create, train, evaluate, and deploy machine learning models effortlessly. It incorporates real-time monitoring with data drift alarms and a feedback mechanism for retraining to ensure model performance remains optimal. It incorporated with a streamlined workflow ensures seamless progression through each stage of the model lifecycle
+                    </p>
+                    <figcaption class="blockquote-footer">Coders Ready<cite title="Source Title"></cite>
+                    </figcaption>
+          
+        </div>
+        <div class="col-lg-3 d-flex align-items-center features-img-bg" data-aos="zoom-out">
+          <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" ><img src="img/ensoVector.png" style="max-width:`100%;max-height:40% ;" class="img-fluid" alt=""></a>
+        </div>
+      </div><!-- Features Item -->
+      </div>
+
+<br>
+
+      <div class="container" data-aos="fade-up">
+
+          <div class="row align-items-center">
+
+            <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="100" data-aos-duration="1000">
+              <h3 class="text-danger">Project 1.</h3>
+              <p class="text-center">
+                <i class="bi bi-quote quote-icon-left"></i>
+                <span>The Deen Dayal UpadhyayaGrameen Kaushalya Yojana (DDU-GKY), Ministry of Rural Development, Govt. of India</span>
+                <i class="bi bi-quote quote-icon-right"></i>
+              </p>
+              
+
+            </div>
+
+            <div class="col-lg-7" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+              <p>
+                <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                <img src="img/project1.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+              </a>
+              </p>
+            </div>
+          </div>
+        </div>
+<br>
+        <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+                        
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="200" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 2.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span> National Institute of Rural Development and Panchayati Raj (NIRD&PR).</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="300" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project2.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+
+                        
+                      </div>
+                    </div>
+<br>
+                    <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 3.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span> NIIT</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="500" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project3.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+              <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="600" data-aos-duration="1000"> 
+                          <h3 class="text-danger">Project 4.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span>Kaushal bharat.gov.in</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="700" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project4.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+<br>
+            <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="800" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 5.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span>IGL  deals in pharmaceutical food and beverage, personal care and industrial sector products</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="900" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project1.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+<br>
+
+            <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="1000" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 6.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span><strong>Watcho OTT </strong>  provides access to a world of OTT apps such as ZEE5, ALT Balaji, Sony Liv, Hungama Play, Watcho and a huge library of curated online videos, Catch-Up shows, and Web-series. </span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                          
+
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="1100" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project6.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <br>
+            <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="1200" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 7.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span>  Next-gen HR services that optimize talent, ignite growth and drive sustainable people performance..</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="1300" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project7.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+              <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="1400" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 8.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span> ATA is a training and certification organization providing globally recognized Certification in the areas of Agile Testing, Kanban, DevOps and technology/tools. </span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="1500" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project8.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+            <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="1600" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 9.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span>Bharatiya Vidya Bhavan’s Vidyashram, NIRD , Rajendranagar,.</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="1700" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project9.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+            <div class="container" data-aos="fade-up">
+
+                      <div class="row align-items-center">
+
+                        <div class="col-lg-5 info" data-aos="fade-up" data-aos-delay="1800" data-aos-duration="1000">
+                          <h3 class="text-danger">Project 10.</h3>
+                          <p class="text-center">
+                            <i class="bi bi-quote quote-icon-left"></i>
+                            <span>The Journal aims at promoting study and research in rural development. It seeks to uncover links between the social sciences and rural development and to forge and strengthen them where necessary.</span>
+                            <i class="bi bi-quote quote-icon-right"></i>
+                          </p>
+                        </div>
+
+                        <div class="col-lg-7" data-aos="fade-up" data-aos-delay="1900" data-aos-duration="1000">
+                          <p>
+                            <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                            <img src="img/project10.png" class="img-fluid" alt="" style="border-radius:10px;" data-aos="fade-in" data-aos-duration="1200">
+                          </a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <br>
+            <div class="container" data-aos="fade-up">
+                    <a style="text-decoration:none;color:inherit;" href="?r=site/portfolio" target="_blank" >
+                  <button type="button" class="btn btn-danger col-sm-12 ">Know us more….. <br> See what we have done</button></a><hr>
+
+                    <div class="row">
+                      <div class="col-md-4">
+
+                        <button type="button" class="btn btn-outline-danger">Case Studies</button>
+                      </div>
+                      <div class="col-md-4">
+                        <button type="button" class="btn btn-outline-danger">Portfolio</button>
+                      </div>
+                      <div class="col-md-4">
+                        <button type="button" class="btn btn-outline-danger">Insight Blog</button>
+                      </div>
+                    </div>
+                  </div>
+
+
+
+    </section><!-- End About Section -->
+  
